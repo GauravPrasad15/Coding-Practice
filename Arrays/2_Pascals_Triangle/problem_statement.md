63d718d8b33873ade7bf25615fe34022734456d6
GauravPrasad15 <gaurav.prasad.kol@gmail.com>
2025-05-27 18:55:04 +0530

    day 2

---
+++ Arrays/2_Pascals_Triangle/problem_statement.md
@@ -0,0 +1,25 @@
+This problem has 3 variations. They are stated below:
+
+Variation 1: Given row number r and column number c. Print the element at position (r, c) in Pascal’s triangle.
+
+Variation 2: Given the row number n. Print the n-th row of Pascal’s triangle.
+
+Variation 3: Given the number of rows n. Print the first n rows of Pascal’s triangle.
+
+In Pascal’s triangle, each number is the sum of the two numbers directly above it as shown in the figure below:
+
+![Image1](https://upload.wikimedia.org/wikipedia/commons/0/0d/PascalTriangleAnimated2.gif "Image1")
+
+Example 1:
+
+Input: numRows = 5
+Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+Example 2:
+
+Input: numRows = 1
+Output: [[1]]
+ 
+
+Constraints:
+
+1 <= numRows <= 30
---
+++ Arrays/2_Pascals_Triangle/test.py
@@ -0,0 +1,51 @@
+from typing import List
+
+class Solution:
+    def generate_pascals_triangle(self, numRows: int) -> List[List[int]]:
+        # creation of new List for storing the matrix
+        List = []
+
+        for i in range(numRows):
+            # arr for storing each new row of the matrix
+            arr = [0]*(i+1)
+            
+            #initializing first and last position in the row with 1
+            arr[0] = 1
+            arr[i] = 1
+
+            # assigning 2 pointers front and back for traversal
+            f = 1
+            b = i-f
+
+            # continuting the loop this back pointer crosses front pointer
+            while f<=b:
+                if f==b:
+                    # List[i-1] - to check in the previous row
+                    arr[f] = List[i-1][f-1] + List[i-1][f]
+
+                    # traversing front pointer 1 step ahead while back pointer 1 step back
+                    f = f+1
+                    b = i-f
+
+                else:
+                    arr[f] = List[i-1][f-1] + List[i-1][f]
+                    arr[b] = List[i-1][b-1] + List[i-1][b]
+                    
+                    # traversing front pointer 1 step ahead while back pointer 1 step back
+                    f = f+1
+                    b = i-f
+
+            # appendind the new row to the matrix        
+            List.append(arr)
+                    
+        return List
+        
+                        
+
+
+class main():
+    solution = Solution()
+    n = 5
+    print(solution.generate_pascals_triangle(n))
+
+main()
