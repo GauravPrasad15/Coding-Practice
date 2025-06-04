70ae5a996d844c966546a51738ecf1ca459be61a
GauravPrasad15 <gaurav.prasad.kol@gmail.com>
2025-06-04 10:30:30 +0530

    day 6

--- Arrays/5_Sort_Colors/optimal.py
+++ Arrays/5_Sort_Colors/optimal.py
@@ -5,7 +5,24 @@ class Solution:
     def sortColors(self, nums: List[int]) -> None:
         low = 0
         mid = 0
-        high = 0
+        high = len(nums) - 1
+
+        for i in range(len(nums)):
+            while mid <= high:
+                if nums[mid]==0:
+                    nums[low], nums[mid] = nums[mid], nums[low]
+                    low+=1
+                    mid+=1
+
+                elif nums[mid]==2:
+                    nums[mid], nums[high] = nums[high], nums[mid]
+                    high-=1
+
+                else:
+                    mid+=1
+
+        print(nums)
+
 
         
 
